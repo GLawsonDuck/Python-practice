644012b9e45fa7dc2b3114166350c8c27b798f27
Gabriel Lawson-Duck <GLawsonDuck@gmail.com>
2023-11-18 15:29:41 +0000

    Add files via upload
    Playing around with physics more, adding notes

--- ExploringRedCircle.py
+++ ExploringRedCircle.py
@@ -8,23 +8,37 @@ pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 
 borders = [40, (screen.get_width() - 40), screen.get_height() - 40, 40]
-border_left = borders[0]
-border_right = borders[1]
-border_bottom = borders[2]
-border_top = borders[3]
+
+border_left, border_right, border_bottom, border_top = borders
 
 
 clock = pygame.time.Clock()
 running = True
 dt = 0
 
+#initial y velocity comes from gravity, y axis is flipped
+gravity = (9.8) * ((720/16.1)) #9.8 m/s * 720 pixels divided by 16.1mm (measured on my screen)
+#currently this value is not interacting with the other values I randomly threw in very well, 
+#so I am using 16.1 rather than the 0.0161 the 16.1mm value should be in future
+yvelocity = gravity
+
+#coefficient of restitution for collisions
+ball_coeff = 0.92
+surface_coeff = 0.98
+
+maxheight = 40 #need this for calculating accel during bounces/falls
+
 class Bounces:
 
     def __init__(self):
         self.num_bounces = 0
+        self.yvelocity = -(yvelocity)*ball_coeff*surface_coeff
+
+    def decay(self):
 
 
-
+        if self.num_bounces > 10:
+            bounced = False
 
 bounce = 0
 bounced = False
@@ -39,17 +53,15 @@ while running:
             running = False
 
     # fill the screen with a color to wipe away anything from last frame
+    
     screen.fill("purple")
 
     pygame.draw.circle(screen, "red", player_pos, 40)
-
-    gravity = 200
 
     player_pos.y += (gravity * dt)
 
     if bounced == True:
         player_pos.y += 15 * (bounce * dt)
-        print(bounce)
         bounce += 60
 
         if bounce >= 0:
@@ -65,7 +77,7 @@ while running:
         player_pos.x -= 300 * dt
     if keys[pygame.K_d]:
         player_pos.x += 300 * dt
-    if keys[pygame.K_SPACE]:
+    if keys[pygame.K_SPACE] and not (keys[pygame.K_SPACE] * (clock.tick())):  #need to read documentation to extinguish jump effect after 1 button press, currently holding space keeps adding to y position
         player_pos.y -= 500 * dt
 
     if player_pos.x < border_left:
@@ -79,6 +91,15 @@ while running:
         bounced = True
         bounce = -300
 
+    if player_pos.y > maxheight: #record max height for bounce physics
+        maxheight = player_pos.y
+        print(maxheight)
+
+    if player_pos.y == 40: #this isn't going to work, but conceptually need a way to reset max height for following bounces or
+        #if player controls are used, cancelling out bounce physics height. New maxheight should depend on "unsupported" height,
+        #i.e. if player controls are used to move to a certain height and then the ball is released, release point will be new
+        #value of maxheight.
+        maxheight = 0
 
         
     # flip() the display to put your work on screen
