e1cd9c575adefa7206bad2a264b8608ec87f5b7a
Gabriel Lawson-Duck <GLawsonDuck@gmail.com>
2023-11-20 16:19:06 +0000

    Add files via upload
    This version works pretty well. It's not perfect by any means, but it's the closest I've come to getting the ball to bounce around and have the appearance of an acceleration from gravity.

--- ExploringRedCircle.py
+++ ExploringRedCircle.py
@@ -17,17 +17,32 @@ running = True
 dt = 0
 
 #initial y velocity comes from gravity, y axis is flipped
-gravity = (9.8) * ((720/16.1)) #9.8 m/s**2 * 720 pixels divided by 16.1mm (measured on my screen)
-#currently this value is not interacting with the other values I randomly threw in very well, 
-#so I am using 16.1 rather than the 0.0161 the 16.1mm value should be in future
-y_accel = gravity #initial acceleration in the y direction at t0 comes purely from gravity
-x_accel = 0 #initial x acceleration is zero
+gravity = (80) #just throwing random numbers in here until the behaviour looks right, not sure how
+#useful it would be to try to derive a number from pixel density etc.
 
 #coefficient of restitution for collisions
 ball_coeff = 0.712 #taken from a website, this is for a tennis ball
-surface_coeff = 0.98
 
-maxheight = (screen.get_height() / 2) #need this for calculating accel during bounces/falls, initial maxheight is init player height
+maxheight_unsupported = (screen.get_height() / 2) #need this for calculating accel during bounces/falls, initial maxheight is init player height
+
+def fall_distance(dt):
+    falldistance = ((gravity)*(dt ** 2))/2 # distance in m an object w/ constant acceleration falls in time t
+
+    return falldistance
+
+def fall_time(distance):
+    falltime = math.sqrt((2 * distance)/gravity) #time it takes to fall a certain distance under constant acceleration
+
+    return falltime
+
+def instantaneous_yvelocity(yVelocity, time, bounced = False):
+    instVelocity = yVelocity + (gravity * time)
+
+    if bounced:
+        pass #figure this out
+
+    return instVelocity
+
 
 def velocity_calc(init_velocity, **accels):
     accel = sum(accels)
@@ -38,10 +53,10 @@ class Bounces:
 
     def __init__(self):
         self.num_bounces = 0
-        self.yvelocity = -(yvelocity)*ball_coeff*surface_coeff
+        self.yvelocity = 0
 
     def decay(self):
-
+        self.yvelocity = -(self.yvelocity)*ball_coeff #the yvelocity needs to be the value from the previous frame
 
         if self.num_bounces > 10:
             bounced = False
@@ -49,7 +64,11 @@ class Bounces:
 bounce = 0
 bounced = False
 
-player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2) 
+
+player_velocity = pygame.Vector2(x = 0, y = gravity * (dt ** 2))
+
+player_accel = pygame.Vector2(x = 0, y = gravity)
 
 while running:
     # poll for events
@@ -61,51 +80,65 @@ while running:
     # fill the screen with a color to wipe away anything from last frame
     
     screen.fill("purple")
-
     pygame.draw.circle(screen, "red", player_pos, 40)
-
-    player_pos.y += (gravity * dt)
-
-    if bounced == True:
-        player_pos.y += 15 * (bounce * dt)
-        bounce += 60
-
-        if bounce >= 0:
-            bounced = False
-
-
+    
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
-        player_pos.y -= 300 * dt
+        #player_pos.y -= 300 * dt
+        maxheight_unsupported = player_pos.y
+        player_velocity.y -= 300 * dt
+        player_accel.y -= 300
+
     if keys[pygame.K_s]:
-        player_pos.y += 300 * dt
+        #player_pos.y += 300 * dt
+        player_velocity.y += 300 * dt
+        player_accel.y += 300
+
     if keys[pygame.K_a]:
-        player_pos.x -= 300 * dt
+        #player_pos.x -= 300 * dt
+        player_velocity.x -= 300 * dt
+
     if keys[pygame.K_d]:
-        player_pos.x += 300 * dt
+        #player_pos.x += 300 * dt
+        player_velocity.x += 300 * dt
+
     if keys[pygame.K_SPACE] and not (keys[pygame.K_SPACE] * (clock.tick())):  #need to read documentation to extinguish jump effect after 1 button press, currently holding space keeps adding to y position
-        player_pos.y -= 500 * dt
+        #player_pos.y -= 500 * dt
+        player_velocity.y -= 500 * dt
 
     if player_pos.x < border_left:
         player_pos.x = border_left
+        bounced = True #do I even need this?
+        player_velocity.x = -(player_velocity.x)*ball_coeff
+
     if player_pos.x > border_right:
         player_pos.x = border_right
-    if player_pos.y < border_top:
-        player_pos.y = border_top
-    if player_pos.y >= border_bottom:
-        player_pos.y -= 600 * dt
-        bounced = True
-        bounce = -300
+        player_velocity.x = -(player_velocity.x)*ball_coeff
 
-    if player_pos.y > maxheight: #record max height for bounce physics
-        maxheight = player_pos.y
-        print(maxheight)
+    if player_pos.y <= border_top:
+        player_pos.y = border_top
+        bounced = True
+        player_velocity.y = -(player_velocity.y)*ball_coeff
+
+    if player_pos.y >= border_bottom:
+        player_pos.y = border_bottom
+        bounced = True
+        player_velocity.y = -(player_velocity.y)*ball_coeff
+
+    player_accel.y += gravity
+
+    player_pos.y += (gravity * dt) + player_velocity.y * dt
+    player_pos.x += player_velocity.x * dt
+
+    if player_pos.y > maxheight_unsupported: #record max height for bounce physics
+        maxheight_unsupported = player_pos.y - 40
+        print(maxheight_unsupported)
 
     if player_pos.y == 40: #this isn't going to work, but conceptually need a way to reset max height for following bounces or
         #if player controls are used, cancelling out bounce physics height. New maxheight should depend on "unsupported" height,
         #i.e. if player controls are used to move to a certain height and then the ball is released, release point will be new
         #value of maxheight.
-        maxheight = 0
+        maxheight_unsupported = 0
 
         
     # flip() the display to put your work on screen
